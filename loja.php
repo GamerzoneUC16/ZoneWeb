21ec90b765c26dc80ac663925644757881043333
Igor Santiago <igor.santiago546@outlook.com>
2023-02-02 22:05:52 -0300

    Ajustando o menu

--- loja.php
+++ loja.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <title>Store Zone</title>
 </head>
 <body>
@@ -12,26 +13,25 @@
         <div class="container-fluid">
             <!-- Agrupamento mobile -->
             <div class="navbar-header">
-                <a href="zonestore.php" class="navbar navbar-brand">
+                <a href="loja.php" class="navbar navbar-brand">
                     <img src="images/GZ2.png" alt="Logotipo Gamerzone">
                 </a>
              </div> 
              <!-- fecha Agrupamento mobile -->
              <!-- Nav direita -->
-             <div class="collapse navbar-collapse" id="menu-publico">
+             
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active">
-                        <a href="loginuser.php">
-                            <span class="bi bi-person-fill"></span>
-                        </a>
-                    </li>
-                    <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Buscar o produto" aria-label="Produto" aria-describedby="basic-addon1">
-                    <li><a href="">icon de Busca</a></li>
-                    </div>
-                    <li><a href="">icon de Carinho</a></li>
-                    <li><a href="">Icon de User</a></li>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Buscar Produto" aria-label="Recipient's username" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><span class="bi bi-search"></span></button>
+                </div>
+                    <li><a href="carinho.php"><span class="bi bi-cart"></span></a></li>
+                    <li><a href="loginuser.php"><span class="bi bi-person-fill"></span></a></li>
+                </ul>
+             </div>
      </nav>
+     <input type="text" class="form-control" placeholder="Buscar o produto" aria-label="Produto" aria-describedby="basic-addon1">  
+     
 </body>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </html>
