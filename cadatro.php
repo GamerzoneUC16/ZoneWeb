1b9b94c3c168808f2d8dd9812ea22bdaa2d8397e
Igor Santiago <igor.santiago546@outlook.com>
2023-02-14 22:42:47 -0300

    Cadastro

--- cadatro.php
+++ cadatro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style2.css">
-    <title>Cadatro</title>
+    <title>Cadatro new head</title>
 </head>
 <body>
     <div class="tela-cadastro">
