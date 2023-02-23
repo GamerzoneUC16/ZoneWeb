ccef2b11a94bfaa7c83e6bb3b5b18c61702d4934
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2023-02-22 22:01:19 -0300

    Alteração quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -12,7 +12,48 @@
     <main class="container">   
     <?php include 'menu.php'?>
     <footer class="panel-footer" style="background: none;">
-    <h3></h3>
+    <h3>A Gamerzone e uma Lan House Gamer e assistência técnica criada em 23 de setembro de 2021 entre um grupo de 5 amigos para possibilitar que os clientes utilizem a tecnologia a favor de suas Gameplay, negócios e entretenimentos.</h3>
+    <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/missao.jpg" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>MISSÃO</h5>
+        <p>Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/visao.jpg" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>VISÃO</h5>
+        <p>Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/valores.jpg" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>VALORES</h5>
+        <p> Transparência, 
+            Entregamos resultados, desempenho e performance
+            Exceder as expectativas dos clientes.
+        </p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     <?php include 'rodape.php'?> 
     </footer>
 </main>
