9e38b085298fa0abdfbd0058585a339d6b5dad07
Vini Callder <caldeiracaiovinicius@gmail.com>
2023-02-26 22:27:00 -0300

    adicionando produto

---
+++ banco/insert-info.sql
@@ -0,0 +1,2 @@
+insert into produtos ( titulo, descricao, resumo, preco, image, destaque, tipos_id) values ( 'Placa Gráfica Nvidia RTX2060', 'Placa traseira toda em metal','KO Ultra Gaming 6GB GDDR6', 2344.99, 'nvidiartx.jpg', 'sim', 1);
+insert into 
