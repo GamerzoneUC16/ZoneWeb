918b57e0889b20ec9b38b7639d1b91589aadab9d
Kauã Pereira da Silva <kauapereira01@hotmail.com>
2023-02-27 23:17:35 -0300

    boa boa

--- inicio.php
+++ inicio.php
@@ -1,62 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="css\style.css">
   <title>GamerZone</title>
 </head>
+
 <body>
-    <div class="container">
-      <header class="topo">
-        <div class="logo_gazerzone">logo</div>
-        <div class="topo-banner">
-          <div class="banner"></div>
-          <nav class="menu">
-            <ul>
-              <li><a href="index.html">Home</a>
-              <li><a href="sobre.html">Sobre nós</a>
-              <li><a href="portofolio.html">Portofolio</a>
-              <li><a href="service.html">Serviços</a>
-              <li><a href="loja.html">Loja</a>
-                <li><a href="contatos.html">Contato</a>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <main class="pricipal">
-        <aside class="lateral"></aside>
-        <section class="conteudo">
-          <div class="caixa">
-            <h4></h4>
-          </div>
-          <div class="caixa">
-            <h4>1</h4>
-          </div>
-          <div class="caixa">
-            <h4>2</h4>
-          </div>
-          <div class="caixa">
-            <h4>3</h4>
-          </div>
-          <div class="caixa">
-            <h4>4</h4>
-          </div>
-          <div class="caixa">
-            <h4>5</h4>
-          </div>
-          <div class="caixa">
-            <h4>6</h4>
-          </div>
-          <div class="caixa">
-            <h4>7</h4>
-          </div>
-        </section>
-      </main>
-      <footer class="rodape">
-        rodape
-      </footer>
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a href="index.php" class="navbar-brand">
+        <img src="images/GZ-NEW.png" alt="Logotipo GamerZone">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active text-light" aria-current="page" href="quemsomos.php">Quem Somos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="loja.php">Loja</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="servicos.php">Serviços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="login.php" tabindex="-1" aria-disabled="false">Cadastre-se | Iniciar sessão</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div class="container">
+
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
-  
-  
--- menu.php
+++ menu.php
@@ -1,46 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css\style.css">
     <title>Menu</title>
 </head>
+
 <body class="fundofixo">
-    <nav class="fixed navbar fixed-top navbar-light bg-light navbar-inverse">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <div class="container-fluid">
-            <div class="navbar-header">
-            <a  href="inicio.php" class="navbar-brand">
-                <img src="images/GZ2_resized.png" alt="Gamer Zone">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a href="index.php" class="navbar-brand">
+                <img src="images/" alt="Logotipo GamerZone">
             </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Loja</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">User</a>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div class="collapsed navbar-collapsed">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active">
-                    <a href="inicio.php">
-                    <span class="bi bi-house-door"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="quemsomos.php ">QUEM SOMOS</a>
-                </li>
-                <li>
-                    <a href="loja.php#LOJA">LOJA</a>
-                </li>
-                <li>
-                    <a href="servicos.php#servicos">SERVIÇOS</a>
-                </li>   
-                <li class="active">
-                    <a href="login.php#login">
-                        <span class="bi bi-people">&nbsp;USER</span>
-                    </a>
-                </li>
-            </ul>
         </div>
     </nav>
 </body>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
 </html>
