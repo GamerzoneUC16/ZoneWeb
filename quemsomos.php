388e2c183e822007703965340f735cb6256c926e
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2023-03-01 22:22:36 -0300

    Alteração quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -4,56 +4,103 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
     <title>Quem Somos</title>
- 
+ <style>
+  h1 {
+    font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: #00BFFF;
+  }
+h2 {
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: #ffffff;
+}
+  .container {
+  display: flex;
+  flex-wrap: wrap;
+}
+/*
+.imagem {
+  flex: 1;
+  margin: 10px;
+  text-align: center;
+}
+
+.imagem img {
+  width: 100%;
+  max-width: 200px;
+  height: auto;
+  margin-bottom: 10px;
+}
+*/
+.titulo {
+  font-family:Georgia, 'Times New Roman', Times, serif;
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 5px;
+  color: #00BFFF;
+}
+
+.subtitulo {
+  font-size: 25px;
+  color: #fdfdfd;
+}
+
+@media screen and (min-width: 768px) {
+  .imagem {
+    flex-basis: calc(33.33% - 20px);
+  }
+}
+.img-1{
+  position: cover;
+  width: 500px;
+  top: 50%;
+  left: 80%;
+  text-align: left;
+}
+.img-2{
+  position: cover;
+  width: 500px;
+  top: 50%;
+  left: 80%;
+  text-align: center;
+}
+.img-3{
+  position: cover;
+  width: 500px;
+  top: 50%;
+  left: 80%;
+  text-align: right;
+}
+ </style>
 </head>
 <body class="fundofixo">
     <main class="container">   
     <?php include 'menu.php'?>
     <footer class="panel-footer" style="background: none;">
-    <h3>A Gamerzone e uma Lan House Gamer e assistência técnica criada em 23 de setembro de 2021 entre um grupo de 5 amigos para possibilitar que os clientes utilizem a tecnologia a favor de suas Gameplay, negócios e entretenimentos.</h3>
-    <div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="images/missao.jpg" class="d-block w-100" alt="">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>MISSÃO</h5>
-        <p>Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="images/visao.jpg" class="d-block w-100" alt="">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>VISÃO</h5>
-        <p>Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="images/valores.jpg" class="d-block w-100" alt="">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>VALORES</h5>
-        <p> Transparência, 
-            Entregamos resultados, desempenho e performance
-            Exceder as expectativas dos clientes.
-        </p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <br>
+    <h1 class="text-center text-uppercase">QUEM SOMOS</h1>
+    <br>
+    <h2>A Gamerzone e uma Lan House Gamer e assistência técnica criada em 23 de setembro de 2021 entre um grupo de 5 amigos para possibilitar que os clientes utilizem a tecnologia a favor de suas Gameplay, negócios e entretenimentos.</h2>
+<br>   
+    <div class="imagem">
+  <img src="images/missao.jpg" alt="" class="img-1">
+  <div class="titulo">Missão</div>
+  <div class="subtitulo">Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</div>
 </div>
+<br>
+<div class="imagem">
+  <img src="images/visao.jpg" alt="" class="img-2">
+  <div class="titulo">Visão</div>
+  <div class="subtitulo">Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</div>
+</div>
+<br>
+<div class="imagem">
+  <img src="images/valores.jpg" alt="" class="img-3">
+  <div class="titulo">Valores</div>
+  <div class="subtitulo">Transparência, entregamos resultados, desempenho e performance, exceder as expectativas dos clientes.</div>
+</div>
+<br>
     <?php include 'rodape.php'?> 
     </footer>
 </main>
@@ -62,4 +109,4 @@
 <script src="js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
 <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
-<script type="text/javascript">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
