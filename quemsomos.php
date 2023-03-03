cf6672882fc349ba8dd3ef54f0dd904ed2cea1b5
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2023-03-03 11:29:39 -0300

    Alteração quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -4,75 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style3.css">
     <title>Quem Somos</title>
- <style>
-  h1 {
-    font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: #00BFFF;
-  }
-h2 {
-    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    color: #ffffff;
-}
-  .container {
-  display: flex;
-  flex-wrap: wrap;
-}
-/*
-.imagem {
-  flex: 1;
-  margin: 10px;
-  text-align: center;
-}
-
-.imagem img {
-  width: 100%;
-  max-width: 200px;
-  height: auto;
-  margin-bottom: 10px;
-}
-*/
-.titulo {
-  font-family:Georgia, 'Times New Roman', Times, serif;
-  font-size: 30px;
-  font-weight: bold;
-  margin-bottom: 5px;
-  color: #00BFFF;
-}
-
-.subtitulo {
-  font-size: 25px;
-  color: #fdfdfd;
-}
-
-@media screen and (min-width: 768px) {
-  .imagem {
-    flex-basis: calc(33.33% - 20px);
-  }
-}
-.img-1{
-  position: cover;
-  width: 500px;
-  top: 50%;
-  left: 80%;
-  text-align: left;
-}
-.img-2{
-  position: cover;
-  width: 500px;
-  top: 50%;
-  left: 80%;
-  text-align: center;
-}
-.img-3{
-  position: cover;
-  width: 500px;
-  top: 50%;
-  left: 80%;
-  text-align: right;
-}
- </style>
 </head>
 <body class="fundofixo">
     <main class="container">   
@@ -83,23 +16,25 @@ h2 {
     <br>
     <h2>A Gamerzone e uma Lan House Gamer e assistência técnica criada em 23 de setembro de 2021 entre um grupo de 5 amigos para possibilitar que os clientes utilizem a tecnologia a favor de suas Gameplay, negócios e entretenimentos.</h2>
 <br>   
-    <div class="imagem">
-  <img src="images/missao.jpg" alt="" class="img-1">
-  <div class="titulo">Missão</div>
-  <div class="subtitulo">Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</div>
-</div>
-<br>
-<div class="imagem">
-  <img src="images/visao.jpg" alt="" class="img-2">
-  <div class="titulo">Visão</div>
-  <div class="subtitulo">Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</div>
-</div>
-<br>
-<div class="imagem">
-  <img src="images/valores.jpg" alt="" class="img-3">
-  <div class="titulo">Valores</div>
-  <div class="subtitulo">Transparência, entregamos resultados, desempenho e performance, exceder as expectativas dos clientes.</div>
-</div>
+<figure class="image1">
+  <img src="images/missao.jpg" class="figure-img img-fluid rounded" alt="">
+  <titulo class = "titulo">Missão</titulo>
+  <br>
+  <subtitulo class="subtitulo">Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</subtitulo>
+</figure>
+<figure class="image2">
+  <img src="images/visao.jpg" class="figure-img img-fluid rounded" alt="">
+  <titulo class = "titulo">Visão</titulo>
+  <br>
+  <subtitulo class="subtitulo">Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</subtitulo>
+</figure>
+<figure class="image3">
+  <img src="images/valores.jpg" class="figure-img img-fluid rounded" alt="">
+  <titulo class = "titulo">Valores</titulo>
+  <br>
+  <subtitulo class="subtitulo">Transparência, entregamos resultados, desempenho e performance, exceder as expectativas dos clientes.
+</subtitulo>
+</figure>
 <br>
     <?php include 'rodape.php'?> 
     </footer>
