8c19cb103476792177bc9cf3d64f1f175fea172f
Victor Gabriel <victorgabriel0303@gmail.com>
2023-03-04 21:47:27 -0300

    inserindo animações na bordas

--- servicos.php
+++ servicos.php
@@ -47,49 +47,155 @@
                 color: #222;
                 padding: 60px;
                 margin: 20px 6px;
+                position: relative;
+           }
+        
+           .servicos article::before {
+            content: "";
+            position: absolute;
+            top: -2px;
+            left: -2px;
+            width: 0;
+            height: 0;
+            background: transparent;
+            border: 2px solid transparent;
+        }
+  
+       .servicos article:hover::before {
+            animation: animate 2s linear forwards;
+        }
+  
+        @keyframes animate {
+            0% {
+                width: 0;
+                height: 0;
+                border-top-color: black;
+                border-right-color: transparent;
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+            }
+  
+            50% {
+                width: 100%;
+                height: 0;
+                border-top-color: black;
+                border-right-color: black;
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+            }
+  
+            100% {
+                width: 100%;
+                height: 100%;
+                border-top-color: black;
+                border-right-color: black;
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+            }
+        }
+  
+        .servicos article::after {
+            content: "";
+            position: absolute;
+            top: -2px;
+            left: -2px;
+            width: 0;
+            height: 0;
+            background: transparent;
+            border: 2px solid transparent;
+        }
+  
+        .servicos article:hover::after {
+            animation: animates 1s linear forwards;
+        }
+  
+        @keyframes animates {
+            0% {
+                width: 0;
+                height: 0;
+                border-top-color: transparent;
+                border-right-color: transparent;
+                border-bottom-color: transparent;
+                border-left-color: black;
+            }
+  
+            50% {
+                width: 0;
+                height: 100%;
+                border-top-color: transparent;
+                border-right-color: transparent;
+                border-bottom-color: black;
+                border-left-color: black;
+            }
+  
+            100% {
+                width: 100%;
+                height: 100%;
+                border-top-color: transparent;
+                border-right-color: transparent;
+                border-bottom-color: black;
+                border-left-color: black;
+            }
+        }
+        .card-body
+           {
+             padding: 50px;
+             background:rgb(11, 55, 179);
            }
 
-           
-           
-           
-           
+           h5
+           {
+            text-align: center;
+            font-size: 30px;
+            
+           }
+     
         </style>
 <body>
     <br>
         <!-- Serviços-->
         <h1>Serviços</h1>
         <p>GamerZone a Melhor LanHouse da Zona Leste!!</p>
-        <div class="text-center">
+        <div class="text-center" id="botao100">
        <span class="bi bi-controller fs-1"></span>
         </div>
-        <br>
-           
         <section class= " servicos " >
             <div class="colunas">
                 <article>
                     <!-- <img src="" alt="">  -->
-                    <span class="bi bi-gear-fill fs-1" ></span>
+                    <div class="icon"><i class="bi bi-gear-fill fs-1"></i></div>
                     <h2>Serviços</h2>
                     <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. </p>
                 </article>
                 <article>
                      <!-- <img src="" alt="">  -->
-                     <i class="bi bi-controller fs-1"></i>
+                     <div class="i"><i class="bi bi-controller fs-1"></i></div>
                     <h2>Jogos</h2>
                     <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. </p>  
                 </article>
                 <article>
                    <!--  <img src="" alt=""> -->
-                   <i class="bi bi-shop fs-1"></i>
+                   <div class="icon"><i class="bi bi-shop fs-1"></i></div>
                     <h2>Vendas</h2>
                     <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. </p>
                 </article>
                 </div>
             </section>
-                <br>
+                
 
                 <!--  implementando o Modal -->
-                <div class="text-center">  
+               
+          
+
+           <!-- borda em processo -->
+           <div class="card bg-info text-black " style="border-radius: 25px;" >
+  <div class="card-body">
+    <h5 class="card-title text-white">Problema com produto ou com sua máquina solicite nosso suporte!</h5>
+    <br>
+              <!-- Final da Borda Implemetado com modal -->
+
+                <!--  implementando o Modal -->
+    <div class="text-center">  
                 <button type="button" class="btn btn-primary btn-lg"  data-bs-toggle="modal" data-bs-target="#chamadomodal" data-bs-whatever="@mdo">Abrir Chamado</button>
                 </div>
 <div class="modal fade" id="chamadomodal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
@@ -143,20 +249,14 @@
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
         <button type="button" class="btn btn-success">Enviar</button>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-           <!-- Final do Modal -->
-
-           <!-- borda em processo -->
-           <div class="card bg-info text-white" style="border-radius: 25px;">
-  <div class="card-body">
-    <h5 class="card-title">Título do card.</h5>
-    <p class="card-text">Texto do card.</p>
-  </div>
+  </div> 
+ </div>
 </div> 
    
+              <!-- Final Modal Implemetado com a borda -->
         
     
 </body>
