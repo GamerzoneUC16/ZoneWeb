7aff8901d67aa994956c118fe783bf60a4dd7dbc
Igor Santiago <igor.santiago546@outlook.com>
2023-03-09 11:46:33 -0300

    Criando carinho

--- carinho.php
+++ carinho.php
@@ -1,12 +1,32 @@
+<?php 
+include 'conn/connect.php';
+$idProd = $_GET['id']; 
+$ProdGet = $conn->query("select * from produtos where id = $idProd");
+$rowProd = $ProdGet->fetch_assoc();
+$numRows = $ProdGet->num_rows;
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
     <title>Carinho</title>
 </head>
-<body>
-    <h1>Melhora esse nome</h1>
+<?php include 'menu.php'; ?>
+<body >
+
+    <img src="images/<?php echo $rowProd['image']; ?>" class="img-responsive img-rounded" >
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </html>
