a48ee734f828956ebbfcaaec65fa1b31499085ec
Kauã Pereira da Silva <kauapereira01@hotmail.com>
2023-03-09 22:51:13 -0300

    Sofrimento ;-;

--- inicio.php
+++ inicio.php
@@ -10,7 +10,7 @@
   <title>GamerZone</title>
 </head>
 
-<body>
+<body class="fundofixo">
   <!-- inicio navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <div class="container-fluid">
@@ -18,7 +18,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <a href="index.php" class="navbar-brand">
-        <img src="images/GZ-NEW.png" alt="Logotipo GamerZone">
+        <img class="img-Logo" src="images/Logo/GamerZone_logo-2-removebg-preview.png" alt="Logotipo GamerZone">
       </a>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
@@ -41,9 +41,9 @@
   <!-- fim navbar -->
   <!-- inicio introdução -->
   <div class="img img-responsive elemento">
-    <h1 class="h1-3 text-light h1-1">GamerZone a LanHouse <br> feita para você</h1>
-    <h3 class="h3-1 text-light">Venha bater aquela gameplay avançada e evoluir suas</h3>
-    <h3 class="h3-2 text-light">mecânicas em equipamentos de alto desempenho!</h3>
+    <h1 class="h1-1 text-light">GamerZone a LanHouse <br> feita para você</h1>
+    <h3 class="h3-1 text-light">Venha bater aquela gameplay avançada e evoluir suas <br>
+      mecânicas em equipamentos de alto desempenho!</h3>
     <br>
     <br>
     <a href="loja.php">
@@ -72,10 +72,10 @@
   </div>
   <div class="container">
     <div class="row">
-      <h4 class="h4-1">CORUJÃO GZH</h4>
-      <h3 class="h3-3">Reserve já</h3>
+      <h4 class="h4-1 text-light">CORUJÃO GZH</h4>
+      <h3 class="h3-3 text-light">Reserve já</h3>
       <div class="col">
-        <h1 class="h1-2">Reserve agora mesmo o seu corujão na GZH, nosso corujão <br> começa às 22 horas e termina às 6 da manhã. <br>
+        <h1 class="h1-2 text-light">Reserve agora mesmo o seu corujão na GZH, nosso corujão <br> começa às 22 horas e termina às 6 da manhã. <br>
           <br>
           Para comprar click no botão abaixo e você será redirecionado <br> para a GZH Store, nossa loja virtual, lá você poderá escolher o <br> mês e o dia que gostaria de reservar!!
         </h1>
@@ -86,57 +86,60 @@
     </div>
   </div>
 
-  <div class="fundoinicio">
+  <div class="card-body">
     <div class="container">
-      <h3 class="h3-4 text-light">A LAN</h3>
-      <h3 class="h3-5 text-light">O desempenho é algo que levamos a sério!</h3>
-      </div>
-    <div class="container space" style="background-color: transparent;">
-      <div class="row">
+      <h3 class="h3-4">A LAN</h3>
+      <h3 class="h3-5">O desempenho é algo que levamos a sério!</h3>
+    </div>
+    <div class="container" style="background-color: transparent;">
+      <div class="row ajuste-1">
         <div class="col">
-          <h3 class=" text-light">Especificações Técnicas</h3>
-          <h3 class=" text-light">Processador Intel i7 8700 <br>
+          <h3 class="h3-6">Especificações Técnicas</h3>
+          <h3 class="h3-11">Processador Intel i7 8700 <br>
             Vídeo RTX 2070 <br>
             SSD 240 GB <br>
             ASUS TUF B360M Plus Gaming <br>
             16 Gb RAM Corsair <br> 3.000Mhz</h3>
         </div>
         <div class="col">
-          <h3 class=" text-light">Monitor e Cadeira</h3>
-          <h3 class=" text-light">Monitor Benq ZOWIE XL2546 240Hz Dyac <br>
+          <h3 class="h3-7 ">Monitor e Cadeira</h3>
+          <h3 class="h3-12">Monitor Benq ZOWIE XL2546 240Hz Dyac <br>
             Cadeiras Gamer DT3 Elise</h3>
         </div>
         <div class="col">
-          <h3 class=" text-light">Periféricos</h3>
-          <h3 class=" text-light">Mouse Zowie ZA12 <br>
+          <h3 class="h3-8">Periféricos</h3>
+          <h3 class="h3-15">Mouse Zowie ZA12 <br>
             HeadSet Corsair HS50 <br>
             Teclado Mecânico CK104 MotoSpeed <br>
             MousePad HyperX <br> Fury S Large</h3>
         </div>
 
         <div class="col">
-          <h3 class=" text-light">Horários</h3>
-          <h3 class=" text-light">Todos os dias <br>
+          <h3 class="h3-9">Horários</h3>
+          <h3 class="h3-13">Todos os dias <br>
             10:00 am - 06:00 am <br>
             Corujão TODOS os dias!</h3>
         </div>
         <div class="col">
-          <h3 class=" text-light">Valores</h3>
-          <h3 class=" text-light">Avulso <br>
+          <h3 class="h3-10">Valores</h3>
+          <h3 class="h3-14">Avulso <br>
             R$ 10,00 <br>
-            Pacote 3 horas <br>
+            Pacote 3 horas
             R$ 27,00 <br>
-            Pacote 6 horas <br>
+            Pacote 6 horas
             R$ 50,00 <br>
-            Pacote 8 horas <br>
+            Pacote 8 horas
             R$ 65,00 <br>
-            Corujão <br>
+            Corujão
             R$ 85,00</h3>
+          <button type="button" class="btn btn-primary button3">Reserve já</button>
         </div>
       </div>
     </div>
   </div>
+
   </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
