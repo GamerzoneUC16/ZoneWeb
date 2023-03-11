023b26cd1e1ab951013973906a14104323979b47
Igor Santiago <igor.santiago546@outlook.com>
2023-03-11 20:08:33 -0300

    Arrumando contatos

--- rodape.php
+++ rodape.php
@@ -8,39 +8,95 @@
 </head>
 <style>
     .img-lg {
-        width: 50%;
-        height: 42%;
+        width: 60%;
+        height: 100%;
     }
 
     .fundo {
-        background-image: linear-gradient(to right, rgb(0, 11, 12),rgb(10, 11, 12),rgb(0, 11, 12));
+        background-image: linear-gradient(to right, rgb(0, 11, 12), rgb(10, 11, 12), rgb(0, 11, 12));
     }
 
     .map-container {
         height: 400px;
     }
+    iframe
+    {
+        margin: 20px 6px;
+    }
 </style>
 
-<body >
+<body>
     <div class="container">
         <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top fundo">
-        <div class="col my-2">
-            <h1>Siga-nos</h1>
-            <a href="https://pt-br.facebook.com/">
-                <span class="bi bi-facebook"></span>
-            </a>
-            <a href="https://wa.me/5511970617855">
-                <span class="bi bi-whatsapp"></span>
-            </a>
-            <a href="">
-                <span class="bi bi-envelope-open"></span>
-            </a>
-            <a href="">
-                <span class="bi bi-github"></span>
-            </a>
-        </div>
+            <!-- Contatos -->
+            <div class="d-grid gap-2">
+                <h1>Siga-nos</h1>
+                <a href="https://pt-br.facebook.com/">
+                    <span class="bi bi-facebook text-muted"> Facebook</span>
+                </a>
+                <a href="https://wa.me/5511970617855">
+                    <span class="bi bi-whatsapp text-muted"> Whatsapp</span>
+                </a>
+                <a href="mailto:gamerzoneUC16@gmail.com?subject=Contato Site&cc=igor.santiago546@outlook.com">
+                    <span class="bi bi-envelope-open text-muted"> Email</span>
+                </a>
+                <a href="https://github.com/GamerzoneUC16">
+                    <span class="bi bi-github text-muted"> Github</span>
+                </a>
+            </div>
+            <!-- Fim Contatos -->
+            <!-- Logo -->
+            <div class="col">
+                <a href="inicio.php" class="d-flex alingn-items-center mb-3 link-dark text-decorantion-none">
+                    <img src="images/Logo/GamerZone_logo-2-removebg-preview.png" class=" img-responsive img-lg  ">
+                </a>
+            </div>
+            <!-- Fim Logo -->
+            <!-- Links -->
+            <div class="col">
+                <h5>Links</h5>
+                <br>
+                <ul class="d-grid gap-2">
+                    <li class="nav-item mb-2">
+                        <a href="quemsomos.php" class="nav-link p-0 text-muted">Quem Somos</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="loja.php" class="nav-link p-0 text-muted">Loja</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="servicos.php" class="nav-link p-0 text-muted">Serviços</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Fim Links -->
+            <!-- Endereço -->
+            <div class="col">
+                <p>A Melhor Gamerhouse da Zona Leste</p>
+                <address>
+                    <p>Av. José Pinheiro Borges - Itaquera, São Paulo - SP, 08220-900</p>
+                    <br>
+                    <span class="bi bi-phone">
+                        &nbsp; (11) 4002-8922
+                    </span>
+                    <br>
+                    <a href="mailto:gamerzoneUC16@gmail.com?subject=Contato Site&cc=igor.santiago546@outlook.com">
+                        <span class="bi bi-envelope-open-fill">
+                            &nbsp; gamerzoneuc16@gmail.com
+                        </span>
+                    </a>
+                </address>
+                <!-- Mapa -->
+                <!-- <div class="col" >
+                    <div class="map-container" >
+                        <div id="map" class="img-fluid" >
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14631.103522626145!2d-46.470643!3d-23.540562!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5e622db57c4f%3A0x3de99bb691d3dc68!2sShopping%20Metr%C3%B4%20Itaquera!5e0!3m2!1spt-BR!2sbr!4v1678574911407!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                        </div>
+                    </div>
+                </div> -->
+                <!-- Fim Mapa -->
+            </div>
+            <!-- Fim Endereço -->
         </footer>
-    </div>  
+    </div>
 </body>
 <script>
     function initMap() {
