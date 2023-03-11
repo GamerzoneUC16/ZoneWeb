a348306a956a02981e787556b575e49c817c3860
Igor Santiago <igor.santiago546@outlook.com>
2023-03-10 23:40:43 -0300

    Continuando o rodapé

--- loja.php
+++ loja.php
@@ -123,7 +123,7 @@ $row_tipos = $ListaTipo->fetch_all();
   </main>
 </body>
 <footer class="panel-footer" style="background-color: none;">
-<?php include ''?>
+<?php include 'rodape.php'?>
 </footer>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- rodape.php
+++ rodape.php
@@ -5,17 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rodapé</title>
 </head>
-<body>
+<style>
+    .img-lg
+    {
+        width: 50%;
+        height: 42%;
+    }
+    .fundo
+    {
+        background-color:darkslategray;
+    }
+</style>
+<body class="fundo">
     <div class="container">
         <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-            <div class="col mb-3">
+            <div class="col mb-6">
                 <a href="inicio.php" class="d-flex alingn-items-center mb-3 link-dark text-decorantion-none">
-                    <img src="images/Logo/GamerZone_logo-2-removebg-preview.png">
+                    <img src="images/Logo/GamerZone_logo-2-removebg-preview.png" class="img-lg">
                 </a>
             </div>
-            <div class="col mb-3"></div>
-            <div class="col mb-3"></div>
-            <div class="col mb-3"></div>
+            <div class="col mb-6">
+                <h5>Links</h5>
+                <br>
+                <ul class=" list-unstyled">
+                    <li class="nav-item mb-2">
+                        <a href="quemsomos.php" class="nav-link p-0 text-muted">Quem Somos</a>
+                    </li>
+                    <br>
+                    <li class="nav-item mb-2">
+                        <a href="loja.php" class="nav-link p-0 text-muted">Loja</a>
+                    </li>
+                    <br>
+                    <li class="nav-item mb-2">
+                        <a href="servicos.php" class="nav-link p-0 text-muted">Serviços</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col mb-6">
+                <div class="embed-responsive embed-responsive-4by3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.776015081373!2d-46.472831885384444!3d-23.540557166708652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5e622db57c4f%3A0x3de99bb691d3dc68!2sShopping%20Metr%C3%B4%20Itaquera!5e0!3m2!1spt-BR!2sbr!4v1675906536421!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="w-100 h-100 position-absolute top-0 start-0 border-0"></iframe>
+                    </div>
+            </div>
+            <div class="col mb-6"></div>
+            <div class="col mb-6">
+            <div class="panel-footer" style="background: none;">
+                <h6 class="text-danger text-center">
+                    Desenvolvido por Gamerzone&trade; 2023
+                    <br>
+                    <a href="http://www.gamerzone.com.br" target="_blank">
+                        gamerzone.com.br
+                    </a>
+                </h6>
+            </div>
+            </div>
         </footer>
     </div>
 </body>
