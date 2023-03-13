ed5f08bab2f478aad657d0cd12316b5dc9ce9595
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2023-03-13 11:26:26 -0300

    Alteração quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -1,61 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style3.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>Quem Somos</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style3.css">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <title>Quem Somos</title>
 </head>
 <body class="fundofixo">
-<?php include 'menu.php'?>
-    <main class="container">   
+  <?php include 'menu.php' ?>
+  <main class="">
     <br>
-    <h1 class="h1-1">QUEM SOMOS</h1>
+    <nav>
+      <br>
+    <h2 class="h2-1 text-info" style="text-align: center;">QUEM SOMOS</h2>
+    </nav>
     <br>
-    <h2 class="h2-2">A Gamerzone e uma Lan House Gamer e assistência técnica criada em 23 de setembro de 2021 entre um grupo de 5 amigos para possibilitar que os clientes utilizem a tecnologia a favor de suas Gameplay, negócios e entretenimentos.</h2> 
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <figure class="image1">
-        <img src="images/missao.jpg" class="figure-img img-fluid rounded" alt="">
-          <titulo class = "titulo">Missão</titulo>
-          <br>
-          <subtitulo class="subtitulo">Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</subtitulo>
+    <h2 class="h2-2">A Gamerzone e uma Lan House Gamer e assistência técnica criada em 23 de setembro de 2021 entre um grupo de 5 amigos para possibilitar que os clientes utilizem a tecnologia a favor de suas Gameplay, negócios e entretenimentos.</h2>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <figure class="image1">
+            <img src="images/missao.jpg" class="figure-img img-fluid rounded" alt="">
+            <titulo class="titulo text-info">Missão</titulo>
+            <br>
+            <subtitulo class="subtitulo">Satisfazer nosso cliente através da tecnologia dos jogos com o maior desempenho e buscar soluções através da melhor manutenção com agilidade e qualidade de serviço.</subtitulo>
           </figure>
-    </div>
-    <div class="col">
-      <figure class="image2">
-        <img src="images/visao.jpg" class="figure-img img-fluid rounded" alt="">
-          <titulo class = "titulo">Visão</titulo>
-          <br>
-          <subtitulo class="subtitulo">Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</subtitulo>
+        </div>
+        <div class="col">
+          <figure class="image2">
+            <img src="images/visao.jpg" class="figure-img img-fluid rounded" alt="">
+            <titulo class="titulo text-info">Visão</titulo>
+            <br>
+            <subtitulo class="subtitulo">Em 5 anos queremos atingir ao mercado de melhor empresa de lan house e assistência técnica em um só lugar dentro do território nacional com visibilidade para os grandes eventos dentro do país no conceito de games e manutenção em hardwares e softwares.</subtitulo>
           </figure>
-    </div>
-    <div class="col">
-      <figure class="image3">
-        <img src="images/valores.jpg" class="figure-img img-fluid rounded" alt="">
-          <titulo class = "titulo">Valores</titulo>
-          <br>
-          <subtitulo class="subtitulo">Transparência, entregamos resultados, desempenho e performance, exceder as expectativas dos clientes.</subtitulo>
+        </div>
+        <div class="col">
+          <figure class="image3">
+            <img src="images/valores.jpg" class="figure-img img-fluid rounded" alt="">
+            <titulo class="titulo text-info">Valores</titulo>
+            <br>
+            <subtitulo class="subtitulo">Transparência, entregamos resultados, desempenho e performance, exceder as expectativas dos clientes.</subtitulo>
           </figure>
+        </div>
+      </div>
     </div>
-  </div>
-</div>        
-</main>
+  </main>
 </body>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-    <footer class="panel-footer" style="background: none;"> 
-    <?php include 'rodape.php'?>
-    </footer>
+<footer class="panel-footer" style="background: none;">
+  <?php include 'rodape.php' ?>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </html>
