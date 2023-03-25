5568f68501afe84f04b65e19f1dc981c0722c9de
Victor1305M <victorgabriel0303@gmail.com>
2023-03-25 02:03:28 -0300

    adicionando os jogos

--- jogos.php
+++ jogos.php
@@ -2,40 +2,40 @@
 <html lang="pt-BR">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/reset.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/reset.css">
   <!--   <link rel="stylesheet" href="css/style.css"> -->
-    <title>Contato</title>
-    </head>
+  <title>Contato</title>
+</head>
 
-    <style>
-
-.fundofixo {
+<style>
+  .fundofixo {
     background: url('images/Fundo/Fundo.jpg') no-repeat fixed;
     -webkit-background-size: cover;
     background-size: cover;
   }
-      h1
-      {
-        text-align: center;
-        font-family:Arial, Helvetica, sans-serif;
-        font-size: 30px;
-        color: white;
-      }
-      h2
-      {
-        font-family:Arial, Helvetica, sans-serif;
-        font-size: 40px;
-      }
-      p
-      {
-        font-family:Arial, Helvetica, sans-serif;
-        font-size: 25px;
-      }
-      .row {
+
+  h1 {
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 50px;
+    color: white;
+  }
+
+  h2 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 40px;
+  }
+
+  p {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 25px;
+  }
+
+  .row {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -43,43 +43,114 @@
 
   .card {
     border-radius: 5px;
-    /* box-shadow: 7px 7px 13px 0px rgba(50, 50, 50, 0.22); */
+    box-shadow: 7px 7px 13px 0px rgba(50, 50, 50, 0.22);
     padding: 20px;
-    margin: 5px;
-    width: 400px;
-    
+    margin: 25px;
+    width: 250px;
+
   }
-    </style>
-    <body class="fundofixo">
+</style>
 
-    <h1 >Catalogo de Jogos</h1>
+<body class="fundofixo">
 
-    <div class="row ">
-   
-   <div class="card ">
-    <img src="images/fortnite.jpg" alt="">
-     <div class="icon"><span class="bi bi-gear-fill fa-2x"></span></div>
-     <h2>Serviços</h2>
-<!--      <p> O  é o  de ferramentas e serviços criados pela Valve que ajudam você a configurar, gerenciar e operar o seu jogo no Steam..</p>
- --></div>
+  <h1>Catalogo de Jogos</h1>
+
+  <div class="row ">
+
+    <div class="card ">
+      <img src="images/jogo/fortnite1.2.jpg" alt="">
+      <h2>Serviços</h2>
+      <!--      <p> O  é o  de ferramentas e serviços criados pela Valve que ajudam você a configurar, gerenciar e operar o seu jogo no Steam..</p>
+ -->
+    </div>
 
 
-<div class="card ">
+    <div class="card ">
+      <img src="images/jogo/call.jpg" alt="">
+      <h2>Vendas</h2>
+      <!--  <p>Venha conferir a nossa variedades de produtos com tudo que você precisa monitor,gabinite,placa de video placa mãe e muito mais!!</p>
+ -->
+    </div>
 
- <h2>Vendas</h2>
- <p>Venha conferir a nossa variedades de produtos com tudo que você precisa monitor,gabinite,placa de video placa mãe e muito mais!!</p>
-</div>
+    <div class="card ">
+      <img src="images/jogo/resi.jpg" alt="">
+      <h2>Jogos</h2>
+      <!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
+ -->
+    </div>
+    <div class="card ">
+      <img src="images/jogo/rocket.jpg" alt="">
+      <h2>Jogos</h2>
+      <!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
+ -->
+    </div>
 
-<div class="card ">
+    <div class="card ">
+      <img src="images/jogo/fifa.jpg" alt="">
 
- <h2>Jogos</h2>
-<!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
- --> 
-</div>
+      <h2>Serviços</h2>
+      <!--      <p> O  é o  de ferramentas e serviços criados pela Valve que ajudam você a configurar, gerenciar e operar o seu jogo no Steam..</p>
+ -->
+    </div>
 
-</div>
+
+    <div class="card ">
+      <img src="images/jogo/god1.jpg" alt="">
+      <h2>Vendas</h2>
+      <!--  <p>Venha conferir a nossa variedades de produtos com tudo que você precisa monitor,gabinite,placa de video placa mãe e muito mais!!</p>
+ -->
+    </div>
+
+    <div class="card ">
+      <img src="images/jogo/last.jpg" alt="">
+      <h2>Jogos</h2>
+      <!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
+ -->
+    </div>
+    <div class="card ">
+      <img src="images/jogo/gta5.jpg" alt="">
+      <h2>Jogos</h2>
+      <!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
+ -->
+
+
+    </div>
+
+    <div class="card ">
+      <img src="images/teste1.jpg" alt="">
+
+      <h2>Serviços</h2>
+      <!--      <p> O  é o  de ferramentas e serviços criados pela Valve que ajudam você a configurar, gerenciar e operar o seu jogo no Steam..</p>
+ -->
+    </div>
+
+
+    <div class="card ">
+      <img src="images/maquinas.jpg" alt="">
+      <h2>Vendas</h2>
+      <!--  <p>Venha conferir a nossa variedades de produtos com tudo que você precisa monitor,gabinite,placa de video placa mãe e muito mais!!</p>
+ -->
+    </div>
+
+    <div class="card ">
+      <img src="images/teste3.jpg" alt="">
+      <h2>Jogos</h2>
+      <!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
+ -->
+    </div>
+    <div class="card ">
+      <img src="images/teste3.jpg" alt="">
+      <h2>Jogos</h2>
+      <!--  <p>Venha ser Diverti com nosso catalogo e variedades de super realista e online não perde tempo vem jogar com a gente e ser diverti na GamerZone </p>
+ -->
+
+
+
+
+
+    </div>
 
 </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </html>
