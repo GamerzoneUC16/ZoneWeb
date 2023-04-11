54b6c5d97bddb905fdc3ce5f182be8f07830477a
Igor Santiago <igor.santiago546@outlook.com>
2023-04-10 21:48:41 -0300

    Mysql Ponto e virgula

--- banco/insert-nivel.sql
+++ banco/insert-nivel.sql
@@ -1,3 +1,3 @@
-insert niveis (sigla,rotulo) values ('sup','suporte');
-insert niveis (sigla,rotulo) values ('adm','administrador');
-insert niveis (sigla,rotulo) values ('com','comum');
+    insert niveis (sigla,rotulo) values ('sup','suporte');
+    insert niveis (sigla,rotulo) values ('adm','administrador');
+    insert niveis (sigla,rotulo) values ('com','comum');
